f4291ba5cc87632d0f724d0c8812a548814ffdda
Norby Baruani <norby.baruani@gmail.com>
2022-10-18 16:28:49 +0200

    update

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	const conferenceName = "GoCon Conference"
@@ -10,4 +12,11 @@ func main() {
 	fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, availableTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var boughtTickets int
+
+	userName = "John Doe"
+	boughtTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, boughtTickets)
 }
